837f358a3ee6b94f3894eb7f0024a1bcae0dd1c4
Jason Xu <jasonxuuuu@gmail.com>
2024-08-02 18:44:51 -0700

    Added comments for the parameters of Announcement

--- app/models.py
+++ app/models.py
@@ -1,5 +1,13 @@
 class Announcements:
     def __init__(self, title, content, timestamp):
+        """
+        Initialize an Announcements object.
+
+        :param title: Title of the announcement.
+        :param content: Body of the announcement.
+        :param timestamp: The time the announcement is created in Epoch time
+        """
+
         self.title = title
         self.content = content
         self.timestamp = timestamp
